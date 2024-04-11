97f94b6e039a3328e1e7ab752c6e0b6be62a1659
RajkumarRangala <rajkumarrangala2@gmail.com>
2024-04-11 22:14:20 +0530

    Portfolio-website

---
+++ education.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+import plotly.graph_objects as go
+import pyscript
+
+
+
+df = {"SSC": 90, "INTERMEDIATE": 89.6, "B.SC(CS)" :70}
+output_div = pyscript.document.print("Hello World")
